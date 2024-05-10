f1f7203215f404ccfa4dfc7dcdefb8134646d1ae
Chris Morrell <inxilpro@users.noreply.github.com>
2024-05-10 14:13:49 -0400

    Fix tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,15 @@
 
 namespace Tests;
 
+use Database\Seeders\GroupSeeder;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use PHPUnit\Framework\Attributes\Before;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+	#[Before]
+	public function seedDefaultGroups(): void
+	{
+		$this->afterApplicationCreated(fn() => $this->seed(GroupSeeder::class));
+	}
 }
