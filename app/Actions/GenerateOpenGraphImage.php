112169bdf5e53afa7a4a7a3130efab9f3ca88169
Chris Morrell <inxilpro@users.noreply.github.com>
2024-05-11 22:03:34 -0400

    Fig OG paths

--- app/Actions/GenerateOpenGraphImage.php
+++ app/Actions/GenerateOpenGraphImage.php
@@ -27,7 +27,7 @@ class GenerateOpenGraphImage
 		$font = new class implements Font {
 			public function path(): string
 			{
-				return storage_path('app/og/fonts/FiraSans-Regular.ttf');
+				return resource_path('og/fonts/FiraSans-Regular.ttf');
 			}
 		};
 		
@@ -36,8 +36,7 @@ class GenerateOpenGraphImage
 			baseFont: $font,
 			baseColor: '#ffffff',
 			backgroundColor: '#000000',
-			background: null,
-			// background: new Theme\Background(storage_path('app/og/images/dots.png')),
+			background: new Theme\Background(resource_path('og/images/dots.png')),
 			borderColor: '#000000',
 			callToActionBackgroundColor: '#000000',
 			callToActionColor: '#ffffff',
@@ -59,7 +58,7 @@ class GenerateOpenGraphImage
 			->url($meetup->group->domain)
 			->title("Meetup @ {$meetup->location}")
 			->description($meetup->range())
-			->watermark(new Theme\Picture(storage_path('app/og/images/phpx.png')))
+			->watermark(new Theme\Picture(resource_path('og/images/phpx.png')))
 			->save($filename);
 		
 		return $filename;
