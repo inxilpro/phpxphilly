a4efd55a6a4d5a09b5f64ca09291551ea2b69c27
Chris Morrell <inxilpro@users.noreply.github.com>
2024-05-16 12:12:40 -0400

    STL

--- database/seeders/GroupSeeder.php
+++ database/seeders/GroupSeeder.php
@@ -9,8 +9,9 @@ class GroupSeeder extends Seeder
 {
 	public function run(): void
 	{
-		$nyc = Group::create([
+		$nyc = Group::updateOrCreate([
 			'domain' => 'phpxnyc.com',
+		], [
 			'name' => 'PHP×NYC',
 			'twitter_url' => 'https://twitter.com/joetannenbaum',
 			'meetup_url' => 'https://www.meetup.com/php-nyc/',
@@ -20,8 +21,9 @@ class GroupSeeder extends Seeder
 		
 		app()->instance('group:phpxnyc.com', $nyc);
 		
-		$philly = Group::create([
+		$philly = Group::updateOrCreate([
 			'domain' => 'phpxphilly.com',
+		], [
 			'name' => 'PHP×Philly',
 			'twitter_url' => 'https://twitter.com/inxilpro',
 			'meetup_url' => 'https://www.meetup.com/php-philly/',
@@ -30,5 +32,17 @@ class GroupSeeder extends Seeder
 		]);
 		
 		app()->instance('group:phpxphilly.com', $philly);
+		
+		$stl = Group::updateOrCreate([
+			'domain' => 'phpxstl.com',
+		], [
+			'name' => 'PHP×STL',
+			'twitter_url' => 'https://twitter.com/skylerkatz',
+			'meetup_url' => 'https://www.meetup.com/php-stl/',
+			'description' => 'A St. Louis-area PHP meetup for web artisans who want to learn and connect.',
+			'og_asset' => 'stl.png',
+		]);
+		
+		app()->instance('group:phpxstl.com', $stl);
 	}
 }
