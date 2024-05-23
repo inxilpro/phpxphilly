01aa6c61bfcecbaf4c08c173a0effe3ce965a30c
Chris Morrell <inxilpro@users.noreply.github.com>
2024-05-23 10:01:58 -0400

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,7 +21,7 @@
 					<path d="M215.4 96H144 107.8 96v8.8V144v40.4 89L.2 202.5c1.6-18.1 10.9-34.9 25.7-45.8L48 140.3V96c0-26.5 21.5-48 48-48h76.6l49.9-36.9C232.2 3.9 243.9 0 256 0s23.8 3.9 33.5 11L339.4 48H416c26.5 0 48 21.5 48 48v44.3l22.1 16.4c14.8 10.9 24.1 27.7 25.7 45.8L416 273.4v-89V144 104.8 96H404.2 368 296.6 215.4zM0 448V242.1L217.6 403.3c11.1 8.2 24.6 12.7 38.4 12.7s27.3-4.4 38.4-12.7L512 242.1V448v0c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64v0zM176 160H336c8.8 0 16 7.2 16 16s-7.2 16-16 16H176c-8.8 0-16-7.2-16-16s7.2-16 16-16zm0 64H336c8.8 0 16 7.2 16 16s-7.2 16-16 16H176c-8.8 0-16-7.2-16-16s7.2-16 16-16z" />
 				</svg>
 			</a>
-			<a href="https://twitter.com/inxilpro" class="group relative" target="_blank">
+			<a href="{{ $group->twitter_url }}" class="group relative" target="_blank">
 				<div class="absolute bg-white whitespace-nowrap px-2 py-1 text-black font-mono font-bold -top-full -left-2 transform transition-all ease-out duration-100 opacity-0 rotate-6 pointer-events-none translate-y-3 group-hover:-rotate-3 group-hover:block group-hover:opacity-100 group-hover:translate-y-0 group-hover:pointer-events-auto">
 					Twitter
 				</div>
@@ -29,7 +29,7 @@
 					<path d="M459.4 151.7c.3 4.5 .3 9.1 .3 13.6 0 138.7-105.6 298.6-298.6 298.6-59.5 0-114.7-17.2-161.1-47.1 8.4 1 16.6 1.3 25.3 1.3 49.1 0 94.2-16.6 130.3-44.8-46.1-1-84.8-31.2-98.1-72.8 6.5 1 13 1.6 19.8 1.6 9.4 0 18.8-1.3 27.6-3.6-48.1-9.7-84.1-52-84.1-103v-1.3c14 7.8 30.2 12.7 47.4 13.3-28.3-18.8-46.8-51-46.8-87.4 0-19.5 5.2-37.4 14.3-53 51.7 63.7 129.3 105.3 216.4 109.8-1.6-7.8-2.6-15.9-2.6-24 0-57.8 46.8-104.9 104.9-104.9 30.2 0 57.5 12.7 76.7 33.1 23.7-4.5 46.5-13.3 66.6-25.3-7.8 24.4-24.4 44.8-46.1 57.8 21.1-2.3 41.6-8.1 60.4-16.2-14.3 20.8-32.2 39.3-52.6 54.3z" />
 				</svg>
 			</a>
-			<a href="https://www.meetup.com/php-philly/" class="group relative" target="_blank">
+			<a href="{{ $group->meetup_url }}" class="group relative" target="_blank">
 				<div class="absolute bg-white whitespace-nowrap px-2 py-1 text-black font-mono font-bold -top-full -left-2 transform transition-all ease-out duration-100 opacity-0 rotate-6 pointer-events-none translate-y-3 group-hover:-rotate-3 group-hover:block group-hover:opacity-100 group-hover:translate-y-0 group-hover:pointer-events-auto">
 					Meetup.com
 				</div>
